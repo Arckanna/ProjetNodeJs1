c91d68137aab2ce090c9c7019d5674a2560c1e43
Arckanna <40278629+Arckanna@users.noreply.github.com>
2022-07-30 12:12:36 +0200

    Update index.js

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://Arcka:aze123@cluster1.i66lp.mongodb.net/twitter?retryWrites=true&w=majority"
+    "mongodb+srv://Arcka:******@cluster1.i66lp.mongodb.net/twitter?retryWrites=true&w=majority"
   )
   .then(() => console.log("connexion db ok"))
   .catch((err) => console.log(err));
