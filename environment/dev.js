2eddb01f136cf4cd34630bdca97da9fd6d0554ea
Arcka <valerie.lecoeur75@gmail.com>
2022-08-03 17:05:32 +0200

    gitignore fix

--- environment/dev.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  dbUrl: "mongodb+srv://Arcka:pointFinal@cluster1.i66lp.mongodb.net/twitter",
-  cert: path.join(__dirname, "../ssl/local.crt"),
-  key: path.join(__dirname, "../ssl/local.key"),
-  portHttp: 3000,
-  portHttps: 3001,
-};
--- environment/production.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  dbUrl: "mongodb+srv://Arcka:pointFinal@cluster1.i66lp.mongodb.net/twitter",
-  cert: path.join(__dirname, ""),
-  key: path.join(__dirname, ""),
-  portHttp: 80,
-  portHttps: 443,
-};
